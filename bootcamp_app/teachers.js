dc8520195245213dd1e0386db15e67ffc96c45a7
Jaemin Han <j4emin.han@gmail.com>
2021-05-27 06:20:42 +0000

    add injection prevention measures

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,6 +7,14 @@ const pool = new Pool({
   database: 'vagrant'
 });
 
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
@@ -16,6 +24,12 @@ JOIN cohorts ON students.cohort_id = cohorts.id
 WHERE cohorts.name LIKE '%${process.argv[2]}%'
 ORDER BY teachers.name
 `)
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
